beb30474e2897ed8c347690ed1669e7a55771992
jeboo <jeb00o@yahoo.com>
2017-01-29 22:01:52 -0500

    Version 1.1: updated hook for Google April '16 changes

--- app/src/main/java/com/none/gsb_destroyer/gsb_destroy.java
+++ app/src/main/java/com/none/gsb_destroyer/gsb_destroy.java
@@ -6,26 +6,30 @@ import android.view.ViewGroup.LayoutParams;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class gsb_destroy implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("com.google.android.googlequicksearchbox"))
-            return;
-
-        findAndHookMethod("com.android.launcher3.SearchDropTargetBar", lpparam.classLoader, "setQsbSearchBar", View.class,
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    }
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        // 0x0 trick from theknut/GEL
-                        View qsBar = (View) param.args[0];
-                        LayoutParams vglp = qsBar.getLayoutParams();
-                        vglp.width = vglp.height =  0;
-                        qsBar.setLayoutParams(vglp);
-                    }
-                });
+        if (lpparam.packageName.equals("com.google.android.googlequicksearchbox"))
+        {
+            findAndHookMethod("com.android.launcher3.Launcher", lpparam.classLoader, "getOrCreateQsbBar",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        }
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            //XposedBridge.log("Inside GSBhook");
+                            // 0x0 trick from theknut/GEL
+                            View qsBar = (View) param.getResult();
+                            LayoutParams vglp = qsBar.getLayoutParams();
+                            vglp.width = vglp.height =  0;
+                            qsBar.setLayoutParams(vglp);
+                        }
+                    });
+        }
     }
+
 }
+
