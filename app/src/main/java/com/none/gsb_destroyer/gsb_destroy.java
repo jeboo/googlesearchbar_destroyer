ffb4ed6ed7fa3484783082658342315cb8bcc58c
jeboo <jeb00o@yahoo.com>
2017-01-29 21:33:01 -0500

    Version 1.0

---
+++ app/src/main/java/com/none/gsb_destroyer/gsb_destroy.java
@@ -0,0 +1,31 @@
+package com.none.gsb_destroyer;
+
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class gsb_destroy implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.google.android.googlequicksearchbox"))
+            return;
+
+        findAndHookMethod("com.android.launcher3.SearchDropTargetBar", lpparam.classLoader, "setQsbSearchBar", View.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    }
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        // 0x0 trick from theknut/GEL
+                        View qsBar = (View) param.args[0];
+                        LayoutParams vglp = qsBar.getLayoutParams();
+                        vglp.width = vglp.height =  0;
+                        qsBar.setLayoutParams(vglp);
+                    }
+                });
+    }
+}
